f97aaa3decb360e10af5818feb4898eed4e25184
funa-1222 <funabashi-ayaka@gmail.com>
2023-06-14 11:37:26 +0900

    静的ファイルの提供

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 //Express Server インスタンスを作成
 const express = require("express");
 const app = express();
+
+//静的ファイルの提供　publicフォルダを指定
+app.use(express.static("public"));
+app.use("/public", express.static("public"));
