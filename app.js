2b2b8a4d6eb22b77bbafb72949e9420dd2169a4f
funa-1222 <funabashi-ayaka@gmail.com>
2023-06-14 11:40:51 +0900

    htmlファイルを表示

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const app = express();
 //静的ファイルの提供　publicフォルダを指定
 app.use(express.static("public"));
 app.use("/public", express.static("public"));
+
+//htmlファイルを表示　/__dirnameはルートディレクトリ
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(3000, () => {
+    console.log("Start Server!");
+});
+
+//http://localhost:3000/
