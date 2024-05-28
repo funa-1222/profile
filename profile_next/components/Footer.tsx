91bb3c9cdd6b55f91bcb93a0bcfae3e5f814a673
船橋彩花 <conan1212aya@gmail.com>
2024-05-29 01:21:43 +0900

    Footerをexport defaultに

--- profile_next/components/Footer.tsx
+++ profile_next/components/Footer.tsx
@@ -1,7 +1,9 @@
-export function Footer() {
+const Footer = () => {
     return (
         <footer>
             <h2>連絡先：funabashi-ayaka@ed.tmu.ac.jp</h2>
         </footer>
     )
 }
+
+export default Footer
