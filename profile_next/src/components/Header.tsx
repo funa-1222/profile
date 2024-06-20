7e8c9aba89445b9d8cf635dc46e41e635819e9d9
船橋彩花 <conan1212aya@gmail.com>
2024-06-20 13:41:00 +0900

    🚧  API叩きたいな

--- profile_next/src/components/Header.tsx
+++ profile_next/src/components/Header.tsx
@@ -3,10 +3,21 @@ import styles from "@/src/components/Header.module.css"
 import Images from "./Images"
 import github from "@/public/imgs/github-mark.png"
 
+import React from 'react'
+
 type titleProps = {
     title: string
 }
+
+// const WEATHER_FORECAST = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=1e52a15d7f2be3f251016d63e908b94d";
+
 const Header: React.FC<titleProps> = (props) => {
+
+    // fetch(WEATHER_FORECAST)
+    //     .then(res => res.json())
+    //     .then(data => data.weather[0].icon)
+    //     .then(id => console.log(id))
+    //     .catch((error) => console.error('Error:', error))
     return (
         <header className={styles.header}>
             <h1 className={styles.title}>{props.title}</h1>
