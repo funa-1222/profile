f30783a2924ff71bd734dcf51f4ad892b55d86e1
船橋彩花 <conan1212aya@gmail.com>
2024-10-10 11:59:25 +0900

    内容更新

--- profile_next/src/components/MyPhoto.tsx
+++ profile_next/src/components/MyPhoto.tsx
@@ -8,7 +8,8 @@ const MyPhoto = () => {
         <div className={styles.img_name}>
             <Images src={me} alt="my photo" />
             <h2>ふなっしー</h2>
-            <p>こんにちは、プログラミング勉強中の動くことが大好き大学院生です！！</p>
+            <p>こんにちは、プログラミング勉強中！</p>
+            <p>動くことが大好き大学院生です！！</p>
         </div>
     )
 }
