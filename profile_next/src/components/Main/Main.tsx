2ec559b3a30176c7fd3d38d1d9da32e5514b816c
船橋彩花 <conan1212aya@gmail.com>
2024-10-12 21:49:29 +0900

    💄 Main CSS追加

--- profile_next/src/components/Main/Main.tsx
+++ profile_next/src/components/Main/Main.tsx
@@ -1,11 +1,12 @@
 import React, { ReactNode } from "react";
+import styles from "./Main.module.css";
 
 interface MainProps {
     children: ReactNode;
 }
 
 const Main: React.FC<MainProps> = ({ children }) => {
-    return (<main>{children}</main>);
+    return (<main className={styles.main}>{children}</main>);
 }
 
 export default Main
